1a0c3a78f955252098a20daae961d28917d8b1ca
gyh <Folov@.github.com>
2018-10-30 20:42:41 +0800

    添加LED灯效果，清理换行符为\n
    1. User\openwrt27 添加LED灯效果
    2. Relay\openwrt25 添加LED灯效果
    3. 整理换行符，全部变为\n
    4. 部分程序可能失效，GitHub并不完全可信

--- 2017-11-1 单5G/Relay/openwrt25/WSOL_Relay.sh
+++ 2017-11-1 单5G/Relay/openwrt25/WSOL_Relay.sh
@@ -21,6 +21,8 @@ mkdir -p /root/bakup
 sleep 2
 kill `ps | grep hostapd | grep -v grep | awk '{print $1}'`
 /root/ap25.sh
+/root/led_config.sh -s sata -t 20 20
+/root/led_config.sh -s usb1 -d off
 
 # 建立向下的路由（阻塞于tcpsrv_down-arm1900）
 /root/tcpsrv_down-arm1900
@@ -32,8 +34,11 @@ while [[ $? -ne 0 ]]; do
 	/root/tcpsrv_down-arm1900
 done
 echo "tcpsrv_down DONE!"
+/root/led_config.sh -s sata -t 60 60
+
 # 修改LPM，配置路由
 /root/link_build_down.sh
+/root/led_config.sh -s sata -t 150 150
 
 # 建立向上的路由
 /root/tcpsrv_up-arm1900 &
@@ -62,6 +67,7 @@ sleep 1
 
 /root/clientAP_25.sh
 sleep 2
+/root/led_config.sh -s sata -t 200 300
 
 # Send LPM to up-hop.
 nexthop_num=`grep '^ssid' /tmp/wsol/bestrouter.txt | sed -r 's/ssid="openwrt(.*)"/\1/'`
@@ -73,6 +79,7 @@ while [[ $? -ne 0 ]]; do
 	/root/tcpcli-arm1900 192.168.$nexthop_num.$nexthop_num /tmp/wsol/LPM_TO_SEND.txt
 done
 echo "tcpcli DONE!"
+/root/led_config.sh -s usb1 -t 20 20
 
 # After getting data from up-hop, tcpsrv_up-arm1900 call link_build_up.sh by system()
 # link_build_up.sh will send LPM_TOP.txt to subnet by tcpcli-arm1900
@@ -84,6 +91,7 @@ while [[ $? -eq 0 ]]; do
 	ps | grep tcpsrv_up-arm1900 | grep -v grep
 done
 echo "tcpsrv_up-arm1900 & DONE!"
+/root/led_config.sh -s usb1 -t 60 60
 
 # Bacup for bestrouter to client
 cp /tmp/wsol/bestrouter.txt /root/bakup/bestrouter.bak
@@ -91,12 +99,15 @@ cp /tmp/wsol/bestrouter.txt /root/bakup/bestrouter.bak
 echo "#!/bin/ash" >| /root/bakup/routebak.sh
 route -n | tail -n +3 | awk '{print "route add -net "$1" gw "$2" netmask "$3" dev "$8}' >> /root/bakup/routebak.sh
 echo "WSOL ESTABLASHED!"
+/root/led_config.sh -s usb1 -t 150 150
 
 # Information Management and Link Maintenance(IMLM)
 echo "IMLM running~"
 sleep 2
 /root/iwscan_trans.sh
 sourcehop_num=`tail -n 1 /tmp/wsol/LPM_TOP.txt | sed 's/openwrt//'`
+/root/led_config.sh -s sata -d on
+/root/led_config.sh -s usb1 -d on
 
 /root/IMLM_RU 192.168.$sourcehop_num.$sourcehop_num $SSID
 while [[ $? -eq 0 ]]; do
--- 2017-11-1 单5G/User/openwrt27/WSOL_User.sh
+++ 2017-11-1 单5G/User/openwrt27/WSOL_User.sh
@@ -21,6 +21,8 @@ kill `ps | grep hostapd | grep -v grep | awk '{print $1}'`
 /root/ap27.sh
 
 echo $SSID >| /tmp/wsol/LPM_TO_SEND.txt
+/root/led_config.sh -s sata -t 20 20
+/root/led_config.sh -s usb1 -d off
 
 # 建立向上的路由
 /root/tcpsrv_up-arm1900 &
@@ -34,6 +36,8 @@ while [ -z "$pid_tcpsrv" ]; do
 	pid_tcpsrv=`ps | grep tcpsrv_up-arm1900 | grep -v grep | awk '{print $1}'`
 done
 echo "tcpsrv_up-arm1900 & OPEN!"
+/root/led_config.sh -s sata -t 60 60
+
 # 寻找AP并接入
 /root/findrouter.sh
 /root/chooserouter-arm1900 /tmp/wsol/routerlist.txt >| /tmp/wsol/bestrouter.txt
@@ -45,9 +49,11 @@ while [[ $? -ne 0 ]]; do
 done
 echo "bestrouter.txt has been written!"
 sleep 1
+/root/led_config.sh -s sata -t 150 150
 
 /root/clientAP_27.sh
 sleep 2
+/root/led_config.sh -s sata -t 200 300
 
 nexthop_num=`grep '^ssid' /tmp/wsol/bestrouter.txt | sed -r 's/ssid="openwrt(.*)"/\1/'`
 /root/tcpcli-arm1900 192.168.$nexthop_num.$nexthop_num /tmp/wsol/LPM_TO_SEND.txt
@@ -57,6 +63,7 @@ while [[ $? -ne 0 ]]; do
 	/root/tcpcli-arm1900 192.168.$nexthop_num.$nexthop_num /tmp/wsol/LPM_TO_SEND.txt
 done
 echo "tcpcli DONE!"
+/root/led_config.sh -s usb1 -t 20 20
 
 # After getting data, tcpsrv_up-arm1900 call link_build_up.sh
 
@@ -67,6 +74,7 @@ while [[ $? -eq 0 ]]; do
 	ps | grep tcpsrv_up-arm1900 | grep -v grep
 done
 echo "tcpsrv_up-arm1900 & DONE!"
+/root/led_config.sh -s usb1 -t 60 60
 
 # Bacup for bestrouter to client
 cp /tmp/wsol/bestrouter.txt /root/bakup/bestrouter.bak
@@ -74,12 +82,17 @@ cp /tmp/wsol/bestrouter.txt /root/bakup/bestrouter.bak
 echo "#!/bin/ash" >| /root/bakup/routebak.sh
 route -n | tail -n +3 | awk '{print "route add -net "$1" gw "$2" netmask "$3" dev "$8}' >> /root/bakup/routebak.sh
 echo "WSOL ESTABLASHED!"
+/root/led_config.sh -s usb1 -t 150 150
+
 
 # Information Management and Link Maintenance(IMLM)
 echo "IMLM running~"
 sleep 2
 /root/iwscan_trans.sh
 sourcehop_num=`tail -n 1 /tmp/wsol/LPM_TOP.txt | sed 's/openwrt//'`
+/root/led_config.sh -s sata -d on
+/root/led_config.sh -s usb1 -d on
+
 
 /root/IMLM_RU 192.168.$sourcehop_num.$sourcehop_num $SSID
 while [[ $? -eq 0 ]]; do
