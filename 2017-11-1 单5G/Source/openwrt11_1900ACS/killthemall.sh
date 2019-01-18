5706a7509b03d99c9ab7d458a7d956e1921bf3bd
gyh <Folov@.github.com>
2019-01-18 14:34:50 +0800

    更新程序
    增加适用于1900ACS以及3200ACM的openwrt11

---
+++ 2017-11-1 单5G/Source/openwrt11_1900ACS/killthemall.sh
@@ -0,0 +1,6 @@
+kill `pgrep wpa_supplicant`
+kill `pgrep hostapd`
+kill `pgrep tcpsrv_up`
+kill `pgrep tcpsrv_down`
+kill `pgrep tcpcli`
+kill `pgrep IMLM`
---
+++ 2017-11-1 单5G/Source/openwrt11_1900ACS/reconnect.sh
@@ -0,0 +1,6 @@
+#!/bin/ash
+
+/root/ap$Self_id.sh
+
+chmod +x /root/bakup/routebak.sh
+/root/bakup/routebak.sh
